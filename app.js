4dccd3d401768ae2ffafce68012593277589ac02
Rohit Banik <rohitbanik.kolkata@gmail.com>
2022-07-15 15:15:24 +0530

    Completed by Rohit Banik

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var time = setInterval(function(){
     var min = String(time.getMinutes()).padStart(2, '0');
     var sec = String(time.getSeconds()).padStart(2, '0');
     
-    topTime.innerHTML = hour + ":" + min + ":" + sec; 
+    topTime.innerHTML = hour + " : " + min + " : " + sec; 
     mainTime.innerHTML = hour + ":" + min; 
     dayDisplay.innerHTML = days[day];
 }, 1000);
